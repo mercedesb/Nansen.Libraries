c80f090b514702a58fd8900e94a60fe1eb47944d
Mercedes Bernard <mercedesbernard@live.com>
2015-05-05 16:23:58 -0500

    added block export example

---
+++ Nansen.Libraries/Examples/BackgroundImageBlockExporter.cs
@@ -0,0 +1,47 @@
+﻿using EPiServer.ContentTransfer;
+using EPiServer.Core;
+using Examples.Models;
+using Examples.Utility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Examples
+{
+	public class BackgroundImageBlockExporter : BlockExcelExporter<BackgroundImageBlock>
+	{
+		public BackgroundImageBlockExporter(List<string> errors, System.Web.HttpResponse response)
+			: base(errors, response) { }
+
+		protected override DataTable GetBlockDataTable(IEnumerable<BackgroundImageBlock> blocks)
+		{
+			DataTable dt = new DataTable();
+			Array values = Enum.GetValues(typeof(BackgroundBlockRows));
+			foreach (BackgroundBlockRows val in values)
+			{
+				dt.Columns.Add(new DataColumn(val.GetDescriptionString(), typeof(string)));
+			}
+
+			foreach (var block in blocks)
+			{
+				DataRow row = dt.NewRow();
+				IContent content = block as IContent;
+
+				row[BackgroundBlockRows.BlockID.GetDescriptionString()] = content.ContentLink.ID.ToString();
+				row[BackgroundBlockRows.Name.GetDescriptionString()] = content.Name;
+				row[BackgroundBlockRows.MainBody.GetDescriptionString()] = GetValue(block.MainBody);
+				row[BackgroundBlockRows.BackgroundImageID.GetDescriptionString()] = GetIDValue(block.BackgroundImage.GetContentReference());
+				row[BackgroundBlockRows.BackgroundMobileImageID.GetDescriptionString()] = GetIDValue(block.BackgroundImageMobile.GetContentReference());
+				row[BackgroundBlockRows.ForegroundImageID.GetDescriptionString()] = GetIDValue(block.ForegroundImage.GetContentReference());
+				row[BackgroundBlockRows.ForegroundImageLeft.GetDescriptionString()] = GetValue(block.PositionForegroundImageLeft);
+				row[BackgroundBlockRows.ForegroundImageTopOffset.GetDescriptionString()] = block.ForegroundImageTopOffset;
+				row[BackgroundBlockRows.ForegroundImageBottomOffset.GetDescriptionString()] = block.ForegroundImageBottomOffset;
+				row[BackgroundBlockRows.ForegroundImageEdgeOffset.GetDescriptionString()] = block.ForegroundImageEdgeOffset;
+
+				dt.Rows.Add(row);
+			}
+
+			return dt;
+		}
+	}
+}
---
+++ Nansen.Libraries/Examples/Utility/Extensions/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+
+namespace Examples.Utility.Extensions
+{
+	public static class UrlExtensions
+	{
+		public static ContentReference GetContentReference(this Url url)
+		{
+			if (url != null)
+				return PermanentLinkUtility.GetContentReference(new UrlBuilder(url));
+
+			return null;
+		}
+	}
+}
