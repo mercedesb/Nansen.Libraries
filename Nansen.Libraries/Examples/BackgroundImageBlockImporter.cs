c541e249799633132c5232323b3a651f9f2eb029
Mercedes Bernard <mercedesbernard@live.com>
2015-05-05 16:20:21 -0500

    created a block import example

---
+++ Nansen.Libraries/Examples/BackgroundImageBlockImporter.cs
@@ -0,0 +1,99 @@
+﻿using EPiServer;
+using EPiServer.ContentTransfer;
+using EPiServer.ContentTransfer.Utility;
+using EPiServer.Core;
+using EPiServer.DataAccess;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using Examples.Models;
+using Examples.Utility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Examples
+{
+	public class BackgroundImageBlockImporter : BlockExcelImporter<BackgroundImageBlock, BackgroundBlockRows>
+	{
+		protected override string CATEGORY_ERROR_KEY { get { return "Category failed"; } }
+		protected override string CREATE_ERROR_KEY { get { return "Create failed"; } }
+		protected override string UPDATE_ERROR_KEY { get { return "Update failed"; } }
+		protected override string ERROR_FORMAT_STRING { get { return "{0} for {1}"; } }
+
+		public BackgroundImageBlockImporter(List<string> errors, int containerRefId, string language = "", bool overwrite = false)
+			: base(errors, containerRefId, language, 0, overwrite) 
+		{ }
+
+		protected override BackgroundImageBlock GetBlockFromRow(string[] blockValues)
+		{
+			string blockID = blockValues[(int)BackgroundBlockRows.BlockID];
+			if (string.IsNullOrWhiteSpace(blockID))
+				return null;
+
+			var contentRef = new ContentReference(int.Parse(blockID));
+			return _contentRepository.Get<BackgroundImageBlock>(contentRef);
+		}
+
+        protected override void HandleUpdate(BackgroundImageBlock clone, string[] blockValues)
+        {
+            SetContentData(clone, blockValues);
+            _contentRepository.Save(clone as IContent, SaveAction.Publish);
+        }
+
+        protected override void SetContentName(BackgroundImageBlock content, string[] contentValues)
+        {
+            var blockContent = content as IContent;
+            blockContent.Name = SetProperty(contentValues[(int)BackgroundBlockRows.Name], blockContent.Name);
+        }
+        
+        protected override void SetContentData(BackgroundImageBlock block, string[] blockValues)
+		{
+			try
+			{
+				var blockContent = block as IContent;
+				blockContent.Name = SetProperty(blockValues[(int)BackgroundBlockRows.Name], blockContent.Name);
+				block.MainBody = SetProperty(new XhtmlString(blockValues[(int)BackgroundBlockRows.MainBody]), block.MainBody);
+				block.BackgroundImage = SetProperty(GetImageUrl(blockValues[(int)BackgroundBlockRows.BackgroundImageID]), block.BackgroundImage);
+				block.BackgroundImageMobile = SetProperty(GetImageUrl(blockValues[(int)BackgroundBlockRows.BackgroundMobileImageID]), block.BackgroundImageMobile);
+				block.ForegroundImage = SetProperty(GetImageUrl(blockValues[(int)BackgroundBlockRows.ForegroundImageID]), block.ForegroundImage);
+				block.PositionForegroundImageLeft = SetProperty(blockValues[(int)BackgroundBlockRows.ForegroundImageLeft].ToLowerInvariant() == bool.TrueString.ToLowerInvariant(), block.PositionForegroundImageLeft);
+				block.ForegroundImageTopOffset = SetProperty(blockValues[(int)BackgroundBlockRows.ForegroundImageTopOffset], block.ForegroundImageTopOffset);
+				block.ForegroundImageBottomOffset = SetProperty(blockValues[(int)BackgroundBlockRows.ForegroundImageBottomOffset], block.ForegroundImageBottomOffset);
+				block.ForegroundImageEdgeOffset = SetProperty(blockValues[(int)BackgroundBlockRows.ForegroundImageEdgeOffset], block.ForegroundImageEdgeOffset);
+			}
+			catch (IndexOutOfRangeException)
+			{
+				//Trailing columns of row were left blank by user
+				//no empty strings were inserted by the code
+				//Should fail silently!!
+			}
+		}
+
+        protected override void SetGlobalProperties(BackgroundImageBlock block, string[] blockValues)
+        {
+        }
+
+		protected override bool CheckExcelHeaders(Stream fileStream)
+		{
+			var firstRow = ExcelUtility.GetExcelFirstRow(fileStream);
+			Array values = Enum.GetValues(typeof(BackgroundBlockRows));
+
+			for (int i = 0; i < values.Length; i++)
+			{
+				if (firstRow[i] != ((BackgroundBlockRows)values.GetValue(i)).GetDescriptionString())
+					return false;
+			}
+			return true;
+		}
+
+		protected virtual Url GetImageUrl(string imageId)
+		{
+			if (!string.IsNullOrWhiteSpace(imageId))
+			{
+				var contentRef = new ContentReference(int.Parse(imageId));
+				return ServiceLocator.Current.GetInstance<UrlResolver>().GetUrl(contentRef);
+			}
+			return string.Empty;
+		}
+	}
+}
---
+++ Nansen.Libraries/Examples/Models/BackgroundImageBlock.cs
@@ -0,0 +1,53 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+using System.ComponentModel.DataAnnotations;
+
+namespace Examples.Models
+{
+	[ContentType(GUID = "FA183632-A914-4159-AA90-29193BBA3D70", DisplayName = "Background Image block")]
+	public class BackgroundImageBlock : BlockData
+	{
+		[CultureSpecific]
+		[Display(Name = "Main body", Order = 100)]
+		public virtual XhtmlString MainBody { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Background image", Order = 150)]
+		[UIHint(UIHint.Image)]
+		public virtual Url BackgroundImage { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Background image mobile", Order = 160)]
+		[UIHint(UIHint.Image)]
+		public virtual Url BackgroundImageMobile { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Mobile image link", Order = 160)]
+		public virtual Url MobileImageLink { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Foreground image", Order = 170)]
+		[UIHint(UIHint.Image)]
+		public virtual Url ForegroundImage { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Position foreground image left", Order = 171)]
+		public virtual bool PositionForegroundImageLeft { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Foreground image top offset", Order = 172)]
+		public virtual string ForegroundImageTopOffset { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Foreground image bottom offset", Order = 173)]
+		public virtual string ForegroundImageBottomOffset { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Foreground image offset from edge", Order = 174)]
+		public virtual string ForegroundImageEdgeOffset { get; set; }
+	}
+}
